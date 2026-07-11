cda7bd27d3d960a054c5a7eb0efef2c6c6086eeb
user1 <kunalsharmakunu09@gmail.com>
2026-07-11 13:24:05 +0530

    new method

--- BACKEND/src/app.js
+++ BACKEND/src/app.js
@@ -3,8 +3,15 @@ const app = express();
 
 const PORT = 3000; 
 
-app.use("/test",(req,res)=>{
-    res.end("YOUR BACKEND API IS RUNNING...");
+app.get("/",(req,res)=>{
+    res.end("HELLOOOOOOOOOO");
+})
+
+app.get("/test/:name/:age",(req,res)=>{
+    const {name,age} = req.params;
+    console.log(name);
+    console.log(age);
+    res.send({firstname:name,age:age});
 }) 
 
 
