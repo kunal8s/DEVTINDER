2be6532761ceef3227bcd80e91bcb701d8986480
user1 <kunalsharmakunu09@gmail.com>
2026-07-11 14:13:03 +0530

    resolve merge conflict

--- BACKEND/src/app.js
+++ BACKEND/src/app.js
@@ -33,24 +33,3 @@ app.get("/test/:name/:age",(req,res)=>{
 app.listen(PORT,()=>{
     console.log("Backend service is running fine. ✅🛩️")
 }) 
-
-const express = require("express");
-const app = express(); 
-
-const PORT = 3000; 
-
-app.get("/",(req,res)=>{
-    res.end("HELLOOOOOOOOOO");
-})
-
-app.get("/test/:name/:age",(req,res)=>{
-    const {name,age} = req.params;
-    console.log(name);
-    console.log(age);
-    res.send({firstname:name,age:age});
-}) 
-
-
-app.listen(PORT,()=>{
-    console.log("Backend service is running fine. ✅🛩️")
-}) 
