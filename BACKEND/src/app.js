5f3f48e4b7163a724f229fea61d44c1aa22d3724
user1 <kunalsharmakunu09@gmail.com>
2026-07-12 10:24:48 +0530

    d

--- BACKEND/src/app.js
+++ BACKEND/src/app.js
@@ -8,6 +8,24 @@ app.get("/",(req,res)=>{
 })
 
 
+// middleware age and country check
+const age_country_check = (req,res,next)=>{
+    const {age,country} = req.params;
+    if(age>=21 && country=="India"){
+        next();
+    }
+    else{
+        res.send({msg:"Unauthorized acess...."})
+    }
+}
+
+// controller 
+const goon = (req,res)=>{
+    res.send("WELCOME TO THE GORGE  !!!!!!!!11")
+};
+
+app.get("/login/:age/:country",age_country_check,goon);
+
 const rh3 = (req,res)=>{
     console.log("external rote 3///")
 }
@@ -30,6 +48,32 @@ app.get("/test/:name/:age",(req,res)=>{
 }) 
 
 
+
+app.get("/checker", (req, res) => {
+  try {
+    // 1. Trigger your test error here
+    throw new Error("TEST_ERROR: hellllooooalalall"); 
+  } catch (error) {
+    // 2. This will safely log to your terminal
+    console.log("Caught test error:", error.message); 
+    
+    // 3. This sends the error back to your browser/Postman so you see it works
+    res.status(500).json({ 
+      success: false, 
+      message: error.message 
+    }); 
+  }
+});
+
+app.use("/ck",(err,req,res,next)=>{
+    if(err){
+        res.status(500).send("hj")
+    }
+})
+
+
 app.listen(PORT,()=>{
     console.log("Backend service is running fine. ✅🛩️")
 }) 
+
+
