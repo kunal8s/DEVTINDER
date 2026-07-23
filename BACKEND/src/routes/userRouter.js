85b5d9f477ea7b7a11c50bfc5821af5510394a53
user1 <kunalsharmakunu09@gmail.com>
2026-07-23 22:33:08 +0530

    pagination

--- BACKEND/src/routes/userRouter.js
+++ BACKEND/src/routes/userRouter.js
@@ -10,7 +10,6 @@ const USER_SAFE_DATA = "name age";
 const userRouter = express.Router();
 
 // GET ALL THE PENDING CONNECTION REQUEST FOR THE LOGGED IN USER
-// GET ALL THE PENDING CONNECTION REQUEST FOR THE LOGGED IN USER
 userRouter.get("/user/requests/received", userAuth, async (req, res) => {
     try { 
         const loggedInUser = req.user;
@@ -54,5 +53,56 @@ userRouter.get("/user/connections",userAuth, async (req,res)=>{
     }
 })
 
+
+userRouter.get("/feed",userAuth, async (req,res)=>{
+    try{
+
+        // user is available to see all user except 
+        // 1. its own profile
+        // 2. already sent connection -> interested
+        // 3. alreadyy in connection -> accepted
+        // 4. Ignored or rejected 
+
+        const loggedInUser = req.user;
+
+        const page = parseInt(req.query.page) || 1;
+        const limit = parseInt(req.query.limit) || 10;
+
+        const skip = (page-1)*limit;
+
+
+
+        // find all people requested (sent + recieved)
+        const connectionRequest = await ConnectionRequest.find({
+            $or:[
+                {fromUserId: loggedInUser._id},{toUserId: loggedInUser._id}
+            ]
+        }).select("fromUserId toUserId");
+
+
+        // this contains already in connection or connection sent and self ID also  
+        const hideUsersFromFeed = new set();
+        connectionRequest.forEach(req=>{
+            hideUsersFromFeed.add(req.fromUserId.toString());
+            hideUsersFromFeed.add(req.toUserId.toString());
+        }) 
+
+        const users = await User.find({
+            $and:[
+                {_id: {$nin: Array.from(hideUsersFromFeed)}},
+                {_id: {$ne:loggedInUser._id}} // for self  
+            ]
+        }).select(USER_SAFE_DATA).skip().limit(limit);
+
+        res.send(users);
+
+
+    }catch(err){
+        res.status(404).json({message: err.message});
+    }
+})
+
+// NOW WE DIDNOT WANT THAT OUR MILLIONS OF DATA BOMBARD AT SAME TIME IN FEED SO WE USED THE PAGONATION DATA COMES IN CHUNK BY CHUNK 
+// .SKIP() & .LIMIT()
 
 module.exports = { userRouter };
